38b43c346251da5cbf3beda3aad5c4414aa7819e
Blake Patches <blakepatches@gmail.com>
2016-01-24 12:18:21 -0500

    Implemented basic github auth

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,5 +1,19 @@
-var github = new Github({
-  username: "YOU_USER",
-  password: "YOUR_PASSWORD",
-  auth: "basic"
+document.addEventListener("DOMContentLoaded", function(event) {
+    var github;
+    function signIn() {
+        var formElement = document.getElementById("form-container");
+        return new Github({
+            username: formElement.querySelector("#gitHubUsername").value,
+            password: formElement.querySelector("#gitHubPassword").value,
+            auth: "basic"
+        });
+    }
+    var signInBtn = document.getElementById("signInBtn");
+    signInBtn.addEventListener("click", function(){
+        github = signIn();
+        var user = github.getUser();
+        user.repos(function(err, repos) {
+            console.log(repos);
+        });
+    }, false);
 });
