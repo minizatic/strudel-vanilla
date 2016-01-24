1863d041e4a6c2c24093d5cd383e11bb8f6a9209
Blake Patches <blakepatches@gmail.com>
2016-01-24 15:01:53 -0500

    Added header & new post UI flow

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,19 +1,43 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var github;
+    
+    var github, user, repo;
+    
     function signIn() {
         var formElement = document.getElementById("form-container");
         return new Github({
-            username: formElement.querySelector("#gitHubUsername").value,
-            password: formElement.querySelector("#gitHubPassword").value,
+            username: formElement.querySelector("#github-username").value,
+            password: formElement.querySelector("#github-password").value,
             auth: "basic"
         });
     }
-    var signInBtn = document.getElementById("signInBtn");
-    signInBtn.addEventListener("click", function(){
+    
+    function loadAdmin() {
         github = signIn();
-        var user = github.getUser();
-        user.repos(function(err, repos) {
-            console.log(repos);
+        user = github.getUser();
+        user.show(null, function(err, user){
+            if(user){
+                document.getElementById("form-container").style.display = "none";
+                document.getElementById("admin-container").style.display = "block";
+                repo = github.getRepo(user.login, "strudel");
+                repo.show(function(err, repo){
+                    console.log(repo);
+                });
+            } else {
+                console.log(err);
+            }
         });
-    }, false);
+    }
+    
+    function newPostView() {
+        document.getElementById("new-post-container").style.display = "block";
+        document.getElementById("new-post-btn").style.display = "none";
+        document.getElementById("edit-posts-btn").style.display = "block";
+    }
+    
+    var signInBtn = document.getElementById("sign-in-btn");
+    signInBtn.addEventListener("click", loadAdmin, false);
+    
+    var newPostBtn = document.getElementById("new-post-btn");
+    newPostBtn.addEventListener("click", newPostView, false);
+    
 });
